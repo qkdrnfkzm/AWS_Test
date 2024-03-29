84ff045621e7c913424350d21c73c092c346f1f8
qkdrnfkzm <bbqcb@naver.com>
2024-03-29 16:46:13 +0900

    Test

---
+++ src/main/java/org/example/aws_test/AwsTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.aws_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aws_test/AwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aws_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
